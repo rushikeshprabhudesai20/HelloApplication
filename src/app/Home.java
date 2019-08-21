c706c1be4ef79fe7263e0d5983d9be7444f9f783
Rushikesh-Prabhudesai <rushikeshprabhudesai20@gmail.com>
2019-08-21 16:02:28 +0530

    stashing done

--- src/app/Home.java
+++ src/app/Home.java
@@ -3,5 +3,7 @@ package app;
 public class Home{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("stashing was done");
+
     }
 }
